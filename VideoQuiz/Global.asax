9f513fc0cbfbd0529c109a0d3542a0571a051fb8
Gazza <ghazaly@wizlearn.com>
2015-02-16 15:44:58 +0800

    Initial commit.

---
+++ VideoQuiz/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VideoQuiz.MvcApplication" Language="C#" %>
---
+++ VideoQuiz/Models/TriviaQuestion.cs
@@ -0,0 +1,19 @@
+﻿// ----------------------------------------------------------------------------------
+// Microsoft Developer & Platform Evangelism
+// 
+// Copyright (c) Microsoft Corporation. All rights reserved.
+namespace GeekQuiz.Models
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class TriviaQuestion
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public virtual List<TriviaOption> Options { get; set; }
+    }
+}
