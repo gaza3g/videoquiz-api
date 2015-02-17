23de479de8eacf4b9ef7873e890a0e66ccd158d1
Gazza <ghazaly@wizlearn.com>
2015-02-17 08:52:05 +0800

    Added our Question model and the generated SP-model file.

---
+++ VideoQuiz/Models/Question.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GeekQuiz.Models
+{
+    public class Question
+    {
+        public int QuestionId { get; set; }
+        public int TypeId {get; set;}
+        public string Title { get; set; }
+    }
+}
